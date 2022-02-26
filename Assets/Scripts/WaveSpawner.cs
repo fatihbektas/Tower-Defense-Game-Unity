82777558900ce4bed0617c9f520bb7acb7808e1a
fatih bektas <fatih.bektass@gmail.com>
2022-02-26 22:51:40 +0300

    Initial commit

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    public float timeBetweenWaves = 5f;
+
+    void Update() 
+    {
+        throw new NotImplementedException();
+    }
+}
