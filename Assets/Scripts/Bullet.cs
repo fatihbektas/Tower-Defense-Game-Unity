b28354f787b552735325f778a2725cb863f62da7
fatih bektas <fatih.bektass@gmail.com>
2022-03-05 15:48:13 +0300

    add missle launcher effect & impact

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,8 +1,10 @@
+using System;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     public float speed = 70f;
+    public float explosionRadius = 0f;
     public GameObject impactEffect;
     private Transform _target;
 
@@ -24,18 +26,52 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(_target);
     }
 
     private void HitTarget()
     {
         var effectInstance = Instantiate(impactEffect, transform.position, transform.rotation);
-        Destroy(effectInstance, 2f);
+        Destroy(effectInstance, 5f);
+
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(_target);
+        }
+        
         Destroy(_target.gameObject);
         Destroy(gameObject);
+    }
+
+    private void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider colliderInExplosionRadius in colliders)
+        {
+            if (colliderInExplosionRadius.CompareTag("Enemy"))
+            {
+                Damage(colliderInExplosionRadius.transform);
+            }
+        }
     }
 
     public void Seek(Transform target)
     {
         _target = target;
+    }
+
+    private void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
